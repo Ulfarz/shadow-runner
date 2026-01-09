22d74723e3510ac19cf3542fbcd63aba03b7dd3e
gueguen.tom <gueguen.tom@gmail.com>
2026-01-09 09:46:09 +0000

    derniere maj

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'mapbox-gl', '@turf/turf', 'zustand', 'idb', 'lucide-react']
+        }
+      }
+    }
+  },
   // @ts-ignore
   test: {
     globals: true,
