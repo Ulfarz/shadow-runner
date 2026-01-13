2a442c81cee2abcdcfd54503214263ee406ae067
Ulfarz <gueguen.tom@gmail.com>
2026-01-13 19:14:52 +0100

    améliorations

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.shadowrunner.game',
   appName: 'Shadow Runner',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '78645863640-ffv1s5ejhc3fhr5tc3idqphbeandpthp.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
